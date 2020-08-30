b77ecc0f041020542a31b7b394c915a6777f706f
senguyen1011 <seantanhnguyen@gmail.com>
2020-08-30 15:16:52 -0400

    Updates

---
+++ precache-manifest.8e81614764ce12c7c646c04e79aab896.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f6cf7050d46ec80ed0701809556532d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "43f739b6edad6790787e",
+    "url": "/portfolio/static/css/main.2d84c9d1.chunk.css"
+  },
+  {
+    "revision": "0d8a2e26702e804c5539",
+    "url": "/portfolio/static/js/2.c397944d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.c397944d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43f739b6edad6790787e",
+    "url": "/portfolio/static/js/main.67d52080.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "94ef040d6d804c0edd0a4e6acc8324c8",
+    "url": "/portfolio/static/media/Resume.94ef040d.pdf"
+  }
+]);
