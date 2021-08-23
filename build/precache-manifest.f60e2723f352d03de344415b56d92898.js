375f0cdbdeb2cdf35f3bc766ceec453d3de30a7a
Jacob Caurdy <caurdyja@msu.edu>
2021-08-22 22:09:39 -0400

    new build

---
+++ build/precache-manifest.f60e2723f352d03de344415b56d92898.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abfaa0bf78209845f46e7982afe9913a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3d00c07d4639d64e8c6c",
+    "url": "/portfolio/static/css/main.c0b56745.chunk.css"
+  },
+  {
+    "revision": "61c05933feb849c92e1e",
+    "url": "/portfolio/static/js/2.e9f41c5d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.e9f41c5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d00c07d4639d64e8c6c",
+    "url": "/portfolio/static/js/main.6d9550a8.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "854c5b70004154aad178d65e2d1b4d07",
+    "url": "/portfolio/static/media/Resume.854c5b70.pdf"
+  }
+]);
