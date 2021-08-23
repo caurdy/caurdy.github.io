43e76a652fcc593e858689b2856eab8e4372b7d9
Jacob Caurdy <caurdyja@msu.edu>
2021-08-22 22:20:56 -0400

    new build

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.c1c2539c235f63400865f14518c507a8.js"
+  "/portfolio/precache-manifest.f60e2723f352d03de344415b56d92898.js"
 );
 
 self.addEventListener('message', (event) => {
