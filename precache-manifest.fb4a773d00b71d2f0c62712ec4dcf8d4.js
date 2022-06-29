18b925619b19a8fbc2256143f921104440c4d3a6
Jacob Caurdy <caurdyja@msu.edu>
2022-06-29 16:56:40 -0400

    Updates

---
+++ precache-manifest.fb4a773d00b71d2f0c62712ec4dcf8d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d174adc0e08085f2a62f6f648ec1d59",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "dfa953deb0c11fa0d071",
+    "url": "/portfolio/static/css/main.510eeb06.chunk.css"
+  },
+  {
+    "revision": "efc3a9d289956a58f1de",
+    "url": "/portfolio/static/js/2.f1341db2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.f1341db2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfa953deb0c11fa0d071",
+    "url": "/portfolio/static/js/main.81f1fcc0.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "7640bdee955b89321b1f9902a9de9d8c",
+    "url": "/portfolio/static/media/Resume.7640bdee.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.19788dbede46439d28dafc15f4265bf5.js"
+  "/portfolio/precache-manifest.fb4a773d00b71d2f0c62712ec4dcf8d4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
