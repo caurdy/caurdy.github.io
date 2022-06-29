d1cbb9d44e6e4dddcf51038de726415552452230
Jacob Caurdy <caurdyja@msu.edu>
2022-06-29 16:50:49 -0400

    Mobile Compatibility Update v1 to Production

---
+++ precache-manifest.19788dbede46439d28dafc15f4265bf5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bf7d7482566c6d44ee02e0a8e64e22e",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "e0b97e9e8245ca92ec87",
+    "url": "/home/static/css/main.510eeb06.chunk.css"
+  },
+  {
+    "revision": "7ab6092b7e98da114e4f",
+    "url": "/home/static/js/2.271f5f91.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/home/static/js/2.271f5f91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0b97e9e8245ca92ec87",
+    "url": "/home/static/js/main.bb9c0cc9.chunk.js"
+  },
+  {
+    "revision": "e3c899f582bcf3bd0bd1",
+    "url": "/home/static/js/runtime-main.c71ad327.js"
+  },
+  {
+    "revision": "7640bdee955b89321b1f9902a9de9d8c",
+    "url": "/home/static/media/Resume.7640bdee.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.ddb2d3a5376b6626a6981d966638129f.js"
+  "/home/precache-manifest.19788dbede46439d28dafc15f4265bf5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
